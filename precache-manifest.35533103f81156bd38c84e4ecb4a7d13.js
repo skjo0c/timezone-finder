2ea6a6f2347ef7449301b0a52b2d240bbd92688b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-12 05:04:39 +0000

    Deploy sbimochan/timezone-finder to github.com/sbimochan/timezone-finder.git:gh-pages

---
+++ precache-manifest.35533103f81156bd38c84e4ecb4a7d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df68fa848e1d144a1fe7",
+    "url": "/timezone-finder/static/css/main.bdffb874.chunk.css"
+  },
+  {
+    "revision": "df68fa848e1d144a1fe7",
+    "url": "/timezone-finder/static/js/main.df68fa84.chunk.js"
+  },
+  {
+    "revision": "c22aefdf0294bd3310f7",
+    "url": "/timezone-finder/static/js/runtime~main.c22aefdf.js"
+  },
+  {
+    "revision": "047e1a873ffd0a8b9227",
+    "url": "/timezone-finder/static/css/2.cfe2619f.chunk.css"
+  },
+  {
+    "revision": "047e1a873ffd0a8b9227",
+    "url": "/timezone-finder/static/js/2.047e1a87.chunk.js"
+  },
+  {
+    "revision": "e603caa568be4898edf7e435cce7fc42",
+    "url": "/timezone-finder/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/timezone-finder/precache-manifest.35533103f81156bd38c84e4ecb4a7d13.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/timezone-finder/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
